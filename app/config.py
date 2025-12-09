9a675138f70db1da23dc006294f6c12969be6894
Amadeo Bon <amadeosph@outlook.com>
2025-12-09 13:48:13 -0300

    Fix config

--- app/config.py
+++ app/config.py
@@ -8,7 +8,10 @@ class Settings:
     POLYGON_API_KEY: str = os.getenv("POLYGON_API_KEY", "")
 
     # Ex: "http://localhost:3000,https://meu-front.vercel.app"
-    FRONTEND_ORIGINS_RAW: str = os.getenv("FRONTEND_ORIGINS", "http://localhost:3000")
+    FRONTEND_ORIGINS_RAW: str = os.getenv(
+        "FRONTEND_ORIGINS",
+        "http://localhost:3000",
+    )
 
     @property
     def frontend_origins(self) -> list[str]:
